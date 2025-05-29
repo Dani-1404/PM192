0accd980ffebcad8b538c5131b3a3e1f3d30dd62
Dani-1404 <018026975@upq.edu.mx>
2025-05-29 17:17:33 -0600

    ejercicio B

--- Practica3/main.js
+++ Practica3/main.js
@@ -4,3 +4,24 @@ import { restar } from './utils.js';
 console.log(restar(20, 2)); 
 console.log(restar(30, 10)); 
 console.log(restar(15, 12));  
+
+
+
+
+function verificarUsuario(usuario) {
+    return new Promise((resolve, reject) => {
+        if (usuario === "admin") {
+            resolve("Acceso concedido");
+        } else {
+            reject("Acceso denegado");
+        }
+    });
+}
+
+verificarUsuario("admin")
+    .then(mensaje => console.log("Resultado admin:", mensaje))
+    .catch(error => console.error("Error admin:", error));
+
+verificarUsuario("usuario123")
+    .then(mensaje => console.log("Resultado usuario123:", mensaje))
+    .catch(error => console.error("Error usuario123:", error));
