c4bbe8d2cbb4ad557671184d34e4e93b2b5269ba
Dani-1404 <018026975@upq.edu.mx>
2025-06-12 18:36:28 -0600

    practica 5

--- my-blank/App.js
+++ my-blank/App.js
@@ -1,20 +1,47 @@
+/* zona 1, aqui van importaciones  */
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Children } from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import React, {useState} from 'react';
 
+const Texto=({style})=>{ //pasamo el parametro style 
+  const[contenido, setContenido]=useState('Hola mundo React')
+  const actualizarTexto=()=>{setContenido('Estado actualizado')}
+  return(
+        <Text style={[styles.text, style]} onPress={actualizarTexto}>{contenido}</Text>) //lo definimos en el componente padre 
+}
+
+/* zona 2 , el main*/
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Texto style={styles.red}>  </Texto>
+      <Texto style={styles.blue}> </Texto>
+      <Texto style={styles.green}>  </Texto>
+      
       <StatusBar style="auto" />
     </View>
   );
 }
 
+
+/* zona 3, estilos */ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
+    alignItems: 'base-line',
     justifyContent: 'center',
-  },
+    flexDirection: 'colum', //cambiamos la posicion de nuestros componentes 
+  },
+text:{
+  color:'white',
+  fontSize:25,
+  //height:100, /*de lo que se tenga disponible*/
+  /*nos va a crear el alto y ancho para cada texto*/
+
+},
+red:{backgroundColor:'red'},
+green:{backgroundColor:'green'},// se creo una clase para que sea de diferente como cada texto 
+blue:{backgroundColor:'blue'}, //agregamos el flex en cada clase 
 });
