fe8565579b02a4c5e48296d11f73bdf93358805f
Dani-1404 <018026975@upq.edu.mx>
2025-06-26 18:48:50 -0600

    PRACTICA 12

--- my-NBlank/App.js
+++ my-NBlank/App.js
@@ -1,20 +1,111 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React,{ useState} from 'react';
+import { StyleSheet, Text, View,TextInput,Button, Alert,Switch,ImageBackground,Image } from 'react-native';
 
 export default function App() {
+  const[nombre,setNombre] = useState('');
+  const[correo,setCorreo] = useState('');
+  const[aceptarTerminos, SetAceptarTerminos] =useState(false);
+
+  const registro = () => {
+    if (!nombre  || !correo ) {
+      Alert.alert('Error', 'Por favor llena todos los campos');
+      return;
+    }
+    if(!aceptarTerminos) {
+       Alert.alert('Error', 'Debes de aceptar los terminos codiciones');
+       return
+    }
+    Alert.alert('¡Registro exitoso!', `Nombre: ${nombre}\nCorreo: ${correo}`);
+  
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+
+    <ImageBackground
+  source={require('./assets/fondo.jpg')}
+>
+  <View style={styles.container}>
+    <Image source={require('./assets/logo.png')} style={styles.logo} />
+    
+
+
+
+    <Text style={styles.titulo}>Registro de Usuario</Text>
+
+    <TextInput
+          style={styles.input}
+          placeholder="Nombre completo"
+          onChangeText={setNombre}
+          value={nombre}
+        />
+      
+       <TextInput
+          style={styles.input}
+          placeholder="Correo electrónico"
+          keyboardType="email-address"
+          onChangeText={setCorreo}
+          value={correo}
+        />
+      
+       <View style={styles.switchContainer}>
+          <Text>Acepto términos y condiciones</Text>
+          <Switch
+            value={aceptarTerminos}
+            onValueChange={SetAceptarTerminos}
+          />
+        </View>
+
+        <Button title="Registrarse" onPress={registro}color="#008080" />
+        <StatusBar style="auto" />
+      </View>
+    </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
+  fondo: {
+    flex: 1,
+    resizeMode: 'cover',
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
+    padding: 20,
     justifyContent: 'center',
+    backgroundColor: 'rgba(255,255,255,0.8)',
+  },
+  logo: {
+    width: 100,
+    height: 100,
+    alignSelf: 'center',
+    marginBottom: 20,
+  },
+  titulo: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+    textAlign: 'center',
+  },
+  input: {
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 10,
+    marginBottom: 15,
+    borderColor: '#ccc',
+    borderWidth: 1,
+  },
+  switchContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 20,
   },
 });
+        
+
+
+
+
+   
+ 
+
